3a1c6920f30f83884d15ab8c2df934ff2439b454
Le Tuan Hung <hunglt@onechain.game>
2023-03-07 16:00:46 +0700

    create file helper

--- android/app/src/main/kotlin/com/example/ai_ecard/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ai_ecard/MainActivity.kt
@@ -1,6 +1,26 @@
 package com.example.ai_ecard
 
+import android.os.Environment
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    val FLUTTER_CHANNEL = "game.onechain.ai_ecard.module/utility"
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            FLUTTER_CHANNEL
+        ).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "downloadDirectory" -> {
+                    result.success(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString())
+                }
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
+    }
 }
---
+++ lib/helper/file_helper.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:screenshot/screenshot.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:pdf/widgets.dart' as pw;
+
+class FileHelper {
+  static Future<void> shareFile({required List<String> files, String? subject, String? text}) async {
+    List<XFile> xFiles = [];
+    String directory;
+    if(Platform.isAndroid){
+      directory = await const MethodChannel('game.onechain.ai_ecard.module/utility').invokeMethod('downloadDirectory');
+    } else {
+      directory =  (await getApplicationDocumentsDirectory()).path;
+    }
+    for (int i = 0; i < files.length; i++) {
+      xFiles.add(XFile('$directory/${files[i]}'));
+    }
+    await Share.shareXFiles(xFiles, subject: subject, text: text);
+  }
+
+  static Future<void> saveFileToStorage(String fileName, Uint8List file) async {
+    String directory;
+    if(Platform.isAndroid){
+      directory = await const MethodChannel('game.onechain.ai_ecard.module/utility').invokeMethod('downloadDirectory');
+    } else {
+      directory =  (await getApplicationDocumentsDirectory()).path;
+    }
+    String filePath = '$directory/$fileName';
+    File fileWriter = File(filePath);
+    await fileWriter.writeAsBytes(file);
+  }
+
+  static Future<void> saveFileToGallery(String fileName, String? albumName, Uint8List file) async {
+    await saveFileToStorage(fileName, file);
+    String directory;
+    if(Platform.isAndroid){
+      directory = await const MethodChannel('game.onechain.ai_ecard.module/utility').invokeMethod('downloadDirectory');
+    } else {
+      directory =  (await getApplicationDocumentsDirectory()).path;
+    }
+    String filePath = '$directory/$fileName';
+
+    if (await File(filePath).exists()) {
+      await GallerySaver.saveImage(filePath, albumName: albumName ?? 'AI-Ecard');
+    }
+  }
+
+  static Future<Uint8List> createImage(Widget image) async {
+    ScreenshotController controller = ScreenshotController();
+    Uint8List file = await controller.captureFromWidget(image);
+    return file;
+  }
+
+  static Future<Uint8List> createPDF(Widget image) async {
+    Uint8List file = await createImage(image);
+    final pdf = pw.Document();
+    pdf.addPage(pw.Page(build: (pw.Context context) {
+      return pw.Center(
+        child: pw.Image(pw.MemoryImage(file), fit: pw.BoxFit.contain),
+      );
+    }));
+    return await pdf.save();
+  }
+}
